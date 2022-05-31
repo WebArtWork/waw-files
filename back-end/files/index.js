bf0af219617dc57aaf78c4ceb08ec161bef46ed1
AlanEagle333 <89319769+AlanEagle333@users.noreply.github.com>
2022-05-31 12:57:22 +0300

    Delete back-end directory

--- back-end/files/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = async function(waw) {
-	const router = waw.router("/api/files/file")
-	bucket.file(imagePath);
-	const file = bucket.file(imagePath);
-	router.post("/file", (req, res) => {
-		req.file
-	});
-	file.save();
-};
